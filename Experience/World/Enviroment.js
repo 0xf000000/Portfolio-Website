99dbb540b35504ef8a74c00fdcf731d47de0d5a6
0xf000000 <Leon@margale.de>
2023-04-18 01:02:14 +0200

    added dark mode

--- Experience/World/Enviroment.js
+++ Experience/World/Enviroment.js
@@ -1,16 +1,37 @@
 import * as THREE from "three"
 import Experience from "../Experience"
 import GSAP from "gsap"
+import GUI from "lil-gui";
 
 export default class Enviroment{
 constructor(){
    this.Experience = new Experience(); 
    this.scene = this.Experience.scene;
-
    this.resources = this.Experience.resources;
 
+   //this.gui = new GUI({ container: document.querySelector(".hero-main")} );
+   this.obj = {
+     colorObj:{ r:0, g: 0, b: 0},
+     intensity: 3,
+   };
+
+
    this.setSunlight();
+  // this.setGUI();
 }
+
+setGUI(){
+   this.gui.addColor(this.obj, "colorObj").onChange(() =>{
+      this.sunLight.color.copy(this.obj.colorObj);
+      this.ambientLight.color.copy(this.obj.colorObj);
+      console.log(this.obj.colorObj);
+   })
+   this.gui.add(this.obj, "intensity", 0,10).onChange(()=>{
+      this.sunLight.intensity = this.obj.intensity;
+      this.sunLight.ambientLight = this.obj.intensity;
+   })
+}
+
 
 setSunlight(){
 this.sunLight = new THREE.DirectionalLight("#ffffff", 1);
@@ -42,15 +63,22 @@ switchTheme(theme)
 {
    if(theme === "dark"){
       GSAP.to(this.sunLight.color,{
-         r: 0 / 255,
-         g: 0 / 255,
-         b: 0 / 255,
+         r: 0.15294117647058825,
+         g: 0.23137254901960785,
+         b: 0.6,
       });
 
       GSAP.to(this.ambientLight.color,{
-         r: 0 / 255,
-         g: 0 / 255,
-         b: 0 / 255,
+         r: 0.15294117647058825,
+          g: 0.23137254901960785,
+           b: 0.6
+      });
+
+      GSAP.to(this.sunLight, {
+         intensity: 0.78,
+      });
+      GSAP.to(this.ambientLight, {
+         intensity: 0.78,
       });
    }else{
       GSAP.to(this.sunLight.color,{
@@ -63,6 +91,13 @@ switchTheme(theme)
          r: 255 / 255,
          g: 255 / 255,
          b: 255 / 255,
+      });
+
+      GSAP.to(this.sunLight, {
+         intensity: 0.78,
+      });
+      GSAP.to(this.ambientLight, {
+         intensity: 0.78,
       });
    }
 }
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -80,6 +80,10 @@ export default class Room{
             }
 
         } );
+        
+
+
+        
 
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11,0.11,0.11);
